76deb6671a5af35f025b5136d412cd8b45eb1e34
RAVICHANDRA <cravi7715@Gmail.com>
2024-08-25 16:25:30 +0530

    u

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,27 +1,33 @@
-import React from 'react'
-import './Hero.css'
-import Picture from '../../assets/Picture.jpg'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
-
+import React from "react";
+import "./Hero.css";
+import Picture from "../../assets/Picture.jpg";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Hero = () => {
   return (
-    <div id='home' className='hero'>
+    <div id="home" className="hero">
       <img src={Picture} alt="" />
       {/* <img src="" alt="" /> */}
-      <h1><span>I'm <br />Ravichandra LS,</span> <br />frontend developer from <br /> India</h1>
-      
-      <div className="hero-action">
-        <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
-        <div className="hero-resume">My Resume</div>
-        <div className="image">
-        
-        </div>
-      </div>
-      
-      
-    </div>
-  )
-}
+      <h1>
+        <span>
+          I'm <br />
+          Ravichandra LS,
+        </span>{" "}
+        <br />
+        frontend developer from <br /> India
+      </h1>
 
-export default Hero
+      <div className="hero-action">
+        <div className="hero-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            Connect With Me
+          </AnchorLink>
+        </div>
+        <div className="hero-resume">My Resume</div>
+        <div className="image"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
