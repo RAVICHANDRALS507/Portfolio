8080b9bd6535c5d1145bc41f5dfc0e9c1a0958bc
RAVICHANDRA <cravi7715@Gmail.com>
2025-01-17 23:51:53 +0530

    About page update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,7 +12,8 @@ const About = () => {
       </div>
       <div className="about-sections">
         <div className="about-left">
-          <img src={profile_img} alt="" />
+          {/* <img src={profile_img} alt="" /> */}
+          <img className="about-img" src="https://imgs.search.brave.com/YM0o-mpXaKXKjYyRmEaL-0hb5kZ7z2rgOHhNYfn6xBk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/cG5nbWFydC5jb20v/ZmlsZXMvNS9Sb2Jv/dC1QTkctSEQucG5n" alt="" />
         </div>
         <div className="about-right">
           <div className="about-para">
